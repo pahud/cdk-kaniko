511fab4e893d7c0521458863cc0c53f9934d3ac3
Pahud Hsieh <pahudnet@gmail.com>
2021-06-08 00:17:32 +0000

    bump projen

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,6 @@
 const { AwsCdkConstructLibrary, DependenciesUpgradeMechanism } = require('projen');
 
-const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN'
+const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
 
 const project = new AwsCdkConstructLibrary({
   author: 'Pahud Hsieh',
