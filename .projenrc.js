360489e912bfea852146c3c8e1ee3510dd87ace5
Pahud Hsieh <pahudnet@gmail.com>
2021-12-13 11:56:35 +0800

    chore: bump cdkv2 (#69)
    update projenrc.js and bump the major release to v2 with `cdkv1` in another branch for daily self-upgrade only.
    
    <a href="https://gitpod.io/#https://github.com/pahud/cdk-kaniko/pull/69"><img src="https://gitpod.io/button/open-in-gitpod.svg"/></a>

--- .projenrc.js
+++ .projenrc.js
@@ -5,7 +5,7 @@ const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Pahud Hsieh',
   authorAddress: 'pahudnet@gmail.com',
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.0.0',
   defaultReleaseBranch: 'main',
   jsiiFqn: 'projen.AwsCdkConstructLibrary',
   name: 'cdk-kaniko',
@@ -13,9 +13,20 @@ const project = new awscdk.AwsCdkConstructLibrary({
   repositoryUrl: 'https://github.com/pahud/cdk-kaniko.git',
   deps: ['cdk-fargate-run-task'],
   peerDeps: ['cdk-fargate-run-task'],
-  devDeps: [
-    '@aws-cdk/assert',
-  ],
+  /**
+   * we default release the main branch(cdkv2) with major version 2.
+   */
+  majorVersion: 2,
+  defaultReleaseBranch: 'main',
+  /**
+    * we also release the cdkv1 branch with major version 1.
+    */
+  releaseBranches: {
+    cdkv1: { npmDistTag: 'cdkv1', majorVersion: 1 },
+  },
+  autoApproveOptions: {
+    secret: 'PROJEN_GITHUB_TOKEN',
+  },
   workflowNodeVersion: '14.17.0',
   depsUpgradeOptions: {
     ignoreProjen: false,
